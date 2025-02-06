56d42ea1efca1314cec0dba479067e6fa3a13a21
FABallemand <allemand.fabien@orange.fr>
2025-02-06 11:42:16 +0100

    Practical Work: - First commit

--- practical_work/README.md
+++ practical_work/README.md
@@ -7,4 +7,7 @@ Instructions for the practical work are detailed in `instructions.md`.
 
 ## Related Links
 
+### Data
+- [Wildfire Prediction Dataset](https://www.kaggle.com/datasets/abdelghaniaaba/wildfire-prediction-dataset/data)
+
 ## Authors
---
+++ practical_work/datasets.py
@@ -0,0 +1,63 @@
+import os
+import glob
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+
+
+class WildfirePredictionDataset(Dataset):
+    """
+    Wildfire prediction dataset.
+    Inspired by: https://github.com/Silectio/Projet-Computer-Vision/blob/main/loadDataset.py
+    """
+    def __init__(
+            self, root_dir="/home/ids/fallemand-24/ROB313/data/wildfire-prediction-dataset",
+            split="train", transform=None):
+        self.root_dir = os.path.join(root_dir, split)
+        self.transform = transform
+        self.image_paths = []
+        self.labels = []
+
+        # Create label to class dictionary
+        self.labels_dict = {
+            0: "nowildfire",
+            1: "wildfire"
+        }
+
+        for cls_idx, cls_name in self.labels_dict.items():
+            folder = os.path.join(self.root_dir, cls_name)
+            for img_path in glob.glob(os.path.join(folder, "*.jpg")):
+                self.image_paths.append(img_path)
+
+                # Remove labels for train split!!!
+                if split == "train":
+                    self.labels.append(-1) 
+                else:
+                    self.labels.append(cls_idx)
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.image_paths[idx]).convert("RGB")
+        label = self.labels[idx]
+        if self.transform:
+            img = self.transform(img)
+        return img, label
+
+    @staticmethod
+    def get_dataloaders(
+            root_dir="/home/ids/fallemand-24/ROB313/data/wildfire-prediction-dataset",
+            transform=None, batch_size=16):
+        train_data = WildfireDataset(root_dir, "train", transform)
+        valid_data = WildfireDataset(root_dir, "valid", transform)
+        test_data = WildfireDataset(root_dir, "test", transform)
+
+        # Split validation set into two subsets
+        valid_data_1, valid_data_2 = torch.utils.data.random_split(valid_data, [0.5, 0.5])
+
+        train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+        valid_loader_1 = DataLoader(valid_data_1, batch_size=batch_size, shuffle=True)
+        valid_loader_2 = DataLoader(valid_data_2, batch_size=batch_size, shuffle=True)
+        test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+
+        return train_loader, valid_loader_1, valid_loader_2, test_loader
